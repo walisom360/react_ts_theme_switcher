88271edf2fe158c99f15b54b5a763a3083c8d27c
walisonmatheus <walisom360@hotmail.com>
2020-04-01 17:48:19 -0300

    first commit

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,10 @@
+export default {
+  title:'dark',
+
+  colors:{
+   primary:'#333',
+   secundary:'#5941A9',
+   background:'#222',
+   text:'#fff'
+  }
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,10 @@
+export default {
+  title:'light',
+
+  colors:{
+   primary:'#EC0868',
+   secundary:'#5941A9',
+   background:'#f5f5f5',
+   text:'#333'
+  }
+}
